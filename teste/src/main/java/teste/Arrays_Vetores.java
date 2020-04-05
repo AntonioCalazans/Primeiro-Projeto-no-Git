b62046cd2f2163cd644807aa9fb33df8771d0085
acalazans <antonio-calazans@hotmail.com>
2020-04-05 02:02:46 -0300

    Add new method

--- teste/src/main/java/teste/Arrays_Vetores.java
+++ teste/src/main/java/teste/Arrays_Vetores.java
@@ -2,6 +2,8 @@ package teste;
 ///Comentário - branch antonio_neto
 public class Arrays_Vetores {
 
+	private static final String FakerAPI = null;
+
 	public static void main(String[] args) {
 
 		int[] a = new int[100];
