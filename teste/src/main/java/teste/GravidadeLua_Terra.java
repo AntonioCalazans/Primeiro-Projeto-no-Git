9d0254ea3c5925e5aa4d7bb651116785c5e41b08
acalazans <antonio-calazans@hotmail.com>
2020-04-05 15:57:24 -0300

    Capacitacao 02

--- teste/src/main/java/teste/GravidadeLua_Terra.java
+++ teste/src/main/java/teste/GravidadeLua_Terra.java
@@ -5,12 +5,14 @@ public class GravidadeLua_Terra {
 	public static void main(String[] args) {
 		
 		double Peso_Da_Lua = 0.17;
+		double Meu_Peso_Na_Lua02 = 0.18;
 		double Meu_Peso_Na_Terra = 98;
 		
 		double Meu_Peso_Na_Lua = Meu_Peso_Na_Terra * Peso_Da_Lua;
+		double Meu_Peso_Na_Lua021 = Meu_Peso_Na_Terra * Peso_Da_Lua;
 		
 		System.out.println("Meu Peso na Lua é de: " + Meu_Peso_Na_Lua);
-
+		System.out.println("Meu Peso na Lua é de: " + Meu_Peso_Na_Lua021);
 	}
 
 }
